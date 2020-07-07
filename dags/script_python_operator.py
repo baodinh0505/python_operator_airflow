e4ecc7f0b5dba21e34addea6c71d232d35af8fed
baodinh0505 <dinnhgiabao@gmail.com>
2020-07-07 19:48:51 +0700

    Update script_python_operator.py

--- dags/script_python_operator.py
+++ dags/script_python_operator.py
@@ -1,12 +1,16 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-
+from airflow.utils.dates import days_ago
 from datetime import datetime
 
+args = {
+    'owner': 'Bao',
+    'start_date': days_ago(2),
+}
 def my_func(**kwargs):
     print(kwargs['item'])
-    return 'Hello Dinoactive'
+    return 'Hello Dinovative'
  
-with DAG('python_dag', description='Python DAG', schedule_interval=None, start_date=datetime(2018, 11, 1),catchup=False) as dag:
-	python_task	= PythonOperator(task_id='python_task', python_callable=my_func,op_kwargs={'item':'Hello - Dinoactive'})
+with DAG('python_dag', default_args=args, schedule_interval=None,catchup=False) as dag:
+	python_task	= PythonOperator(task_id='python_task', python_callable=my_func,op_kwargs={'item':'Hello - Dinovative'})
  
